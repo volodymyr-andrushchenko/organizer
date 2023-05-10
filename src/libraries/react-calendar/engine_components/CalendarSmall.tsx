5611c6cb39716b908857eec24d531c6bfdd6e49c
Volodymyr Andrushchenko <v.andrushchenko@seedium.io>
2023-05-10 20:43:56 +0300

    can render miniature calendar on the left|

--- src/libraries/react-calendar/engine_components/CalendarSmall.tsx
+++ src/libraries/react-calendar/engine_components/CalendarSmall.tsx
@@ -1,249 +1,204 @@
-import React, { useState, useContext, useEffect, useMemo } from "react"
+import { useState, useContext } from "react"
 import { CalendarContext } from "../common/CalendarContext"
-import clsx from "clsx"
-import { makeStyles, Theme } from "@material-ui/core/styles"
-import { injectIntl } from "react-intl"
-import { i18nPreviousLabel, i18nNextLabel } from "../common/i18nLabels"
+import { styled } from '@mui/system'
 import { format, getMonth, addMonths, subMonths } from "date-fns"
-import Grid from "@material-ui/core/Grid"
-import Tooltip from "@material-ui/core/Tooltip"
-import IconButton from "@material-ui/core/IconButton"
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
-import ChevronRightIcon from "@material-ui/icons/ChevronRight"
-import TodayOutlinedIcon from "@material-ui/icons/TodayOutlined"
-import Typography from "@material-ui/core/Typography"
-import { lightBlue } from "@material-ui/core/colors"
-import getWeekDays from "../common/getWeekDays"
+import Grid from "@mui/material/Grid"
+import Tooltip from "@mui/material/Tooltip"
+import IconButton from "@mui/material/IconButton"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import ChevronRightIcon from "@mui/icons-material/ChevronRight"
+import Typography, { TypographyProps } from "@mui/material/Typography"
+import { lightBlue } from "@mui/material/colors"
 
-const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        minHeight: 264,
-        minWidth: 240,
-        background: theme.palette.background.paper,
-    },
-    title: {
-        marginLeft: theme.spacing(1),
-        textTransform: "capitalize",
-    },
+const Section = styled('section')({
+    minHeight: 264,
+    minWidth: 240,
+    background: 'white',
+})
 
-    dayHeader: {
-        textAlign: "center",
-        fontSize: 12,
-        color: lightBlue[800],
-        lineHeight: "26px",
-        padding: theme.spacing(0.2),
-        borderColor: theme.palette.background.paper,
-        borderStyle: "solid",
-        textTransform: "capitalize",
-        background: theme.palette.background.paper,
-        height: 34.3,
-        width: 34.3,
-    },
-    day: {
-        textAlign: "center",
-        fontSize: 12,
-        cursor: "pointer",
-        borderRadius: "50%",
-        borderWidth: theme.spacing(0.4),
-        lineHeight: "26px",
-        padding: theme.spacing(0.2),
-        background: theme.palette.background.paper,
-        height: 34.3,
-        width: 34.3,
-    },
-    today: {
-        color: theme.palette.background.paper,
-        background: theme.palette.background.paper,
-        borderColor: theme.palette.background.paper,
-        borderStyle: "solid",
-        backgroundColor: lightBlue[700],
-        "&:hover": {
-            backgroundColor: lightBlue[800],
-        },
-    },
-    notToday: {
-        background: theme.palette.background.paper,
-        borderColor: theme.palette.background.paper,
-        borderStyle: "solid",
-        "&:hover": {
-            backgroundColor: theme.palette.grey[100],
-        },
-    },
-    selected: {
-        color: "#ffffff",
-        borderColor: theme.palette.background.paper,
-        borderStyle: "solid",
-        backgroundColor: lightBlue[500],
-        "&:hover": {
-            color: "#ffffff",
-            backgroundColor: lightBlue[600],
-        },
-    },
-    notCurrentMonth: {
-        color: theme.palette.grey[500],
-        background: theme.palette.background.paper,
-    },
-    navigation: {
-        marginRight: theme.spacing(0.5),
-    },
-    tooltip: {
-        marginTop: 2,
-    },
-    todayButton: {
-        marginRight: 2,
-    },
-    todayIcon: {
-        fontSize: "1.5rem",
-        padding: 2,
-    },
+const Title = styled(Typography)({
+    marginLeft: 8,
+    textTransform: "capitalize",
+})
+
+const Navigation = styled(Grid)({
+    marginRight: 4,
+})
+
+const DayHeader = styled(Typography)({
+    textAlign: "center",
+    fontSize: 12,
+    color: lightBlue[800],
+    lineHeight: "26px",
+    padding: 2,
+    borderColor: 'white',
+    borderStyle: "solid",
+    textTransform: "capitalize",
+    background: 'white',
+    height: 34.3,
+    width: 34.3,
+})
+
+interface StyledDayProps extends TypographyProps {
+    selected?: boolean
+    today?: boolean
+    isCurrentMonth?: boolean
+}
+
+const Day = styled(Typography, {
+    shouldForwardProp: (propName) => propName !== 'selected' && propName !== 'today' && propName !== 'isCurrentMonth',
+})<StyledDayProps>(({ selected, today, isCurrentMonth }) => ({
+    textAlign: "center",
+    fontSize: 12,
+    cursor: "pointer",
+    borderRadius: "50%",
+    border: '1px solid white',
+    lineHeight: "26px",
+    padding: 4,
+    height: 34.3,
+    width: 34.3,
+
+    color: selected || today ? 'white' :
+        !isCurrentMonth ? '#888'
+            : 'black',
+
+    background:
+        selected ? lightBlue[500]
+            : today ? lightBlue[700]
+                : 'white',
+
+    '&:hover': {
+        opacity: .8
+    }
 }))
 
-function CalendarSmall(props: any) {
-    const classes = useStyles()
-    const { intl, isDatepicker = false, datepickerOnChange = () => {}, datepickerValue } = props
+
+function getDatesAroundProvidedDate(providedDate: Date): Date[] {
+    const tenDaysBefore = [];
+    for (let i = 1; i <= 10; i++) {
+        const date = new Date(providedDate.getTime() - i * 24 * 60 * 60 * 1000);
+        tenDaysBefore.push(date);
+    }
+
+    const thirtyOneDaysAfter = [];
+    for (let i = 1; i <= 31; i++) {
+        const date = new Date(providedDate.getTime() + i * 24 * 60 * 60 * 1000);
+        thirtyOneDaysAfter.push(date);
+    }
+
+    return [...tenDaysBefore.reverse(), providedDate, ...thirtyOneDaysAfter];
+}
+
+function breakArrayIntoChunksOfSeven<T>(array: T[]) {
+    const subarrays = [];
+    for (let i = 0; i < array.length; i += 7) {
+        subarrays.push(array.slice(i, i + 7));
+    }
+    return subarrays;
+}
+
+function CalendarSmall() {
     const { stateCalendar, setStateCalendar } = useContext(CalendarContext)
-    const { selectedDate, locale } = stateCalendar
+    const { selectedDate } = stateCalendar
 
-    const [internalDate, setInternalDate] = useState(isDatepicker ? datepickerValue : selectedDate)
-    const [selectedInternalDate, setSelectedInternalDate] = useState(isDatepicker ? datepickerValue : null)
+    const [internalDate, setInternalDate] = useState(selectedDate)
+    const [selectedInternalDate, setSelectedInternalDate] = useState(selectedDate)
 
-    useEffect(() => {
-        // console.group("CalendarSmall : useEffect")
-        // console.log(selectedDate)
-        // console.log(selectedInternalDate)
-        // console.groupEnd()
-        setInternalDate(isDatepicker ? datepickerValue : selectedDate)
-        !isDatepicker && selectedDate !== selectedInternalDate && setSelectedInternalDate("")
-    }, [selectedDate, selectedInternalDate, isDatepicker, datepickerValue])
+    const weeks = breakArrayIntoChunksOfSeven(getDatesAroundProvidedDate(internalDate))
 
-    return useMemo(() => {
-        // console.log('small...')
-        const weeks = getWeekDays(internalDate, 7)
+    function MoveMonths(direction: '<' | '>') {
+        setInternalDate(direction === "<" ? subMonths(internalDate, 1) : addMonths(internalDate, 1))
+    }
 
-        const findNewDate = (props: any) => {
-            const { direction } = props
-            setInternalDate(direction === "<" ? subMonths(internalDate, 1) : addMonths(internalDate, 1))
-        }
+    function selectDate(newDate: Date) {
+        setStateCalendar({ ...stateCalendar, selectedDate: newDate })
+        setSelectedInternalDate(newDate)
+    }
 
-        const selectDate = (props: any) => {
-            const { newDate } = props
-
-            if (!isDatepicker) {
-                setStateCalendar({ ...stateCalendar, selectedDate: newDate })
-                setSelectedInternalDate(newDate)
-            } else {
-                datepickerOnChange(newDate)
-            }
-        }
-
-        return (
-            <section className={classes.root}>
-                <Grid container direction='row' justify='flex-end' alignItems='center' spacing={0} wrap='nowrap'>
-                    <Grid item xs={8}>
-                        <Typography className={classes.title}>
-                            {format(internalDate, "MMMM YYYY", { locale: locale })}
-                        </Typography>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={4}
-                        container
-                        direction='row'
-                        justify='flex-end'
-                        alignItems='center'
-                        spacing={0}
-                        wrap='nowrap'
-                        className={classes.navigation}
-                    >
-                        {isDatepicker && (
-                            <Tooltip
-                                title={`${format(new Date(), "dddd, D MMMM", { locale: locale })}`}
-                                classes={{ tooltip: classes.tooltip }}
-                            >
-                                <IconButton
-                                    size='small'
-                                    aria-label='Today'
-                                    onClick={() => {
-                                        setInternalDate(new Date())
-                                    }}
-                                    className={classes.todayButton}
-                                >
-                                    <TodayOutlinedIcon className={classes.todayIcon} />
-                                </IconButton>
-                            </Tooltip>
-                        )}{" "}
-                        <Tooltip
-                            title={intl.formatMessage({ id: i18nPreviousLabel("month") })}
-                            classes={{ tooltip: classes.tooltip }}
-                        >
-                            <IconButton size='small' onClick={event => findNewDate({ event, direction: "<" })}>
-                                <ChevronLeftIcon />
-                            </IconButton>
-                        </Tooltip>{" "}
-                        <Tooltip
-                            title={intl.formatMessage({ id: i18nNextLabel("month") })}
-                            classes={{ tooltip: classes.tooltip }}
-                        >
-                            <IconButton size='small' onClick={event => findNewDate({ event, direction: ">" })}>
-                                <ChevronRightIcon />
-                            </IconButton>
-                        </Tooltip>
-                    </Grid>
+    return (
+        <Section>
+            <Grid container direction='row' justifyContent='flex-end' alignItems='center' spacing={0} wrap='nowrap'>
+                <Grid item xs={8}>
+                    <Title>
+                        {format(internalDate, "MMMM yyyy")}
+                    </Title>
                 </Grid>
+                <Navigation
+                    item
+                    xs={4}
+                    container
+                    direction='row'
+                    justifyContent='flex-end'
+                    alignItems='center'
+                    spacing={0}
+                    wrap='nowrap'
+                >
+                    <Tooltip
+                        title='Previous Months'
+                    >
+                        <IconButton size='small' onClick={() => MoveMonths("<")}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </Tooltip>{" "}
+                    <Tooltip
+                        title='Next Months'
+                    >
+                        <IconButton size='small' onClick={() => MoveMonths(">")}>
+                            <ChevronRightIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Navigation>
+            </Grid>
 
-                <Grid container spacing={0} direction='row' justify='center' alignItems='center' wrap='nowrap'>
-                    {weeks[0].map((weekDay: Date, index: number) => {
+            <Grid container spacing={0} direction='row' justifyContent='center' alignItems='center' wrap='nowrap'>
+                {weeks[0].map((weekDay: Date, index: number) => {
+                    return (
+                        <Grid item xs key={`small-calendar-column-header-${index}`}>
+                            <DayHeader>
+                                {format(weekDay, "dd")}
+                            </DayHeader>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+
+            {weeks.map((week: Date[], weekIndex: number) => (
+                <Grid
+                    container
+                    spacing={0}
+                    direction='row'
+                    justifyContent='center'
+                    alignItems='center'
+                    wrap='nowrap'
+                    key={`small-calendar-line-${weekIndex}`}
+                >
+                    {week.map((day: Date, dayIndex: number) => {
+                        const isToday = format(day, "ddMMyyyy") === format(new Date(), "ddMMyyyy")
+
+                        const isSelected =
+                            selectedInternalDate !== null &&
+                            !isToday &&
+                            format(day, "ddMMyyyy") === format(selectedInternalDate, "ddMMyyyy")
+
+                        const isCurrentMonth = getMonth(internalDate) === getMonth(day)
+
                         return (
-                            <Grid item xs key={`small-calendar-column-header-${index}`}>
-                                <Typography className={classes.dayHeader}>
-                                    {format(weekDay, "dd", { locale: locale }).substr(0, 1)}
-                                </Typography>
+                            <Grid item xs key={`small-calendar-line-${weekIndex}-column-${dayIndex}`}>
+                                <Day
+                                    selected={isSelected}
+                                    today={isToday}
+                                    isCurrentMonth={isCurrentMonth}
+                                    onClick={() => selectDate(day)}
+                                >
+                                    {day.getDate()}
+                                </Day>
                             </Grid>
                         )
                     })}
                 </Grid>
-
-                {weeks.map((week: any, weekIndex: number) => (
-                    <Grid
-                        container
-                        spacing={0}
-                        direction='row'
-                        justify='center'
-                        alignItems='center'
-                        wrap='nowrap'
-                        key={`small-calendar-line-${weekIndex}`}
-                    >
-                        {week.map((day: any, dayIndex: number) => {
-                            const isToday = format(day, "DDMMYYYY") === format(new Date(), "DDMMYYYY")
-                            const isSelected =
-                                selectedInternalDate !== null &&
-                                !isToday &&
-                                format(day, "DDMMYYYY") === format(selectedInternalDate, "DDMMYYYY")
-
-                            const isCurrentMonth = getMonth(internalDate) === getMonth(day)
-
-                            return (
-                                <Grid item xs key={`small-calendar-line-${weekIndex}-column-${dayIndex}`}>
-                                    <Typography
-                                        className={clsx(classes.day, {
-                                            [classes.today]: isToday,
-                                            [classes.notToday]: !isToday,
-                                            [classes.selected]: !isToday && isSelected,
-                                            [classes.notCurrentMonth]: !isCurrentMonth,
-                                        })}
-                                        onClick={() => selectDate({ newDate: day })}
-                                    >
-                                        {day.getDate()}
-                                    </Typography>
-                                </Grid>
-                            )
-                        })}
-                    </Grid>
-                ))}
-            </section>
-        )
-        // eslint-disable-next-line
-    }, [classes, internalDate, locale, selectedInternalDate])
+            ))}
+        </Section>
+    )
 }
 
-export default injectIntl(CalendarSmall)
+export default CalendarSmall
